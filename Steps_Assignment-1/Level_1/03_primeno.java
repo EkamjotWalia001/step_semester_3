25ce81e0cab79bb7292d930ce5f3cae6afdc260e
EkamjotWalia001 <ew4376@srmist.edu.in>
2026-08-29 12:49:44 +0530

    Add P3 class to print numbers up to N

---
+++ Steps_Assignment-1/Level_1/03_primeno.java
@@ -0,0 +1,10 @@
+class P3 {
+    static void printNumbersUpToN(int n) {
+        for (int i = 1; i <= n; i++)
+            System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        printNumbersUpToN(5);
+    }
+}
